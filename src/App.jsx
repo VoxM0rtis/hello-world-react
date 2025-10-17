1eabf1706fc5d7c47e341f06e24e6210b8245658
VoxM0rtis <maksim.medunica@gmail.com>
2025-10-18 00:51:03 +0700

    Initial commit: Hello World React app with components

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from './components/Button';
+import Container from './components/Container';
+import Navigation from './components/Navigation';
+import './App.css';
+
+function App() {
+  const handleHelloClick = () => {
+    alert('Hello World!');
+  };
+
+  return (
+    <div className="App">
+      <Navigation />
+      <Container>
+        <h1>Hello World!</h1>
+        <p>Это простое React-приложение с кастомными компонентами.</p>
+        <Button onClick={handleHelloClick}>Нажми меня!</Button>
+        <Button variant="secondary">Вторичная кнопка</Button>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+const Button = ({ children, onClick, variant = 'primary' }) => {
+  return (
+    <button className={`button button--${variant}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
